3db04674d1c3e0329b02eddd5d0f8ba4ce4fd29a
davyt23@hotmail.co.uk <davyt23@hotmail.co.uk@b7b788c1-5c5b-4f29-a009-63fcb0e5f780>
2010-12-25 19:46:25 +0000

    *Initial commit 2/3 -Add original racing game files
    git-svn-id: https://pl3.projectlocker.com/Pepsi1x1/RacingGame/svn@2 b7b788c1-5c5b-4f29-a009-63fcb0e5f780

---
+++ RacingGameWindows1/RacingGameContentProcessors/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Racing Game Content Processors")]
+[assembly: AssemblyProduct("RacingGameContentProcessors")]
+[assembly: AssemblyDescription("Content processor for Racing Game.")]
+[assembly: AssemblyCompany("$registeredorganization$")]
+[assembly: AssemblyCopyright("Copyright © $registeredorganization$ $year$")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("041ce288-1ee9-4495-9aa7-a584e291ae4c")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+
