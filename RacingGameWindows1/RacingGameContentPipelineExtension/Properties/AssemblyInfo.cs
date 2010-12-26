67aa42c9002c9eed943250afefe5e356a94f5a2e
davyt23@hotmail.co.uk <davyt23@hotmail.co.uk@b7b788c1-5c5b-4f29-a009-63fcb0e5f780>
2010-12-26 11:50:25 +0000

    *Replace Content Processor project with Content Pipeline Extension project
    git-svn-id: https://pl3.projectlocker.com/Pepsi1x1/RacingGame/svn@4 b7b788c1-5c5b-4f29-a009-63fcb0e5f780

--- RacingGameWindows1/RacingGameContentPipelineExtension/Properties/AssemblyInfo.cs
+++ RacingGameWindows1/RacingGameContentPipelineExtension/Properties/AssemblyInfo.cs
@@ -5,11 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Racing Game Content Processors")]
-[assembly: AssemblyProduct("RacingGameContentProcessors")]
-[assembly: AssemblyDescription("Content processor for Racing Game.")]
-[assembly: AssemblyCompany("$registeredorganization$")]
-[assembly: AssemblyCopyright("Copyright © $registeredorganization$ $year$")]
+[assembly: AssemblyTitle("RacingGameContentPipelineExtension")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RacingGameContentPipelineExtension")]
+[assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,8 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("041ce288-1ee9-4495-9aa7-a584e291ae4c")]
-
+[assembly: Guid("2e42fb75-dc7c-42fb-95d5-de5e3198fdf9")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
-
+[assembly: AssemblyFileVersion("1.0.0.0")]
